05dfee45eea762f7ea0101bae5fd339fa6253b80
rafaeltmbr <rafaeltmbr@gmail.com>
2019-12-21 17:54:01 -0300

    add two new methods to Util

--- src/script/Util.js
+++ src/script/Util.js
@@ -6,4 +6,31 @@ export default class Util {
     static removeListenerToEvents(target, events, eventHandler, options = {}) {
         events.forEach((event) => target.removeEventListener(event, eventHandler, options));
     }
+
+    static isAncestor(ancestor, element) {
+        if (!element || !ancestor) return false;
+
+        for (let e = element.parentElement; e; e = e.parentElement) {
+            if (e === ancestor) {
+                return true;
+            }
+        }
+        return false;
+    }
+
+    static isDescendant(descendant, element) {
+        return Util.isAncestor(element, descendant);
+    }
+
+    static selectAncestors(element, elementsList) {
+        if (!elementsList || !elementsList.filter) return [];
+
+        return elementsList.filter((e) => Util.isAncestor(e, element));
+    }
+
+    static selectDescendants(element, elementsList) {
+        if (!elementsList || !elementsList.filter) return [];
+
+        return elementsList.filter((e) => Util.isDescendant(e, element));
+    }
 }
