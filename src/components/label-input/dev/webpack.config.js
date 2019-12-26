37cccf6479a3226a605e8f10093d82b319a21524
rafaeltmbr <rafaeltmbr@gmail.com>
2019-12-26 16:48:05 -0300

    components subfolder renamed

---
+++ src/components/label-input/dev/webpack.config.js
@@ -0,0 +1,16 @@
+module.exports = {
+    entry: __dirname + '/index.js',
+    output: {
+        path: __dirname + '../build',
+        filename: 'bundle.js'
+    },
+    module: {
+        rules: [
+            {
+                test: /\.js/,
+                use: 'babel-loader',
+                exclude: '/node_modules/'
+            }
+        ]
+    }
+};
